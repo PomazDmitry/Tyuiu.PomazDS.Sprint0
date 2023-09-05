308f9954a259171386c39f441197e831ce7c8ddf
iwsjlt <zed-n.v@mail.ru>
2023-09-05 19:56:51 +0500

    Task4

---
+++ Tyuiu.PomazDS.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PomazDS.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PomazDS.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("27619b66-3730-46fc-9c9f-57447f138bfe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PomazDS.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PomazDS.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.PomazDS.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(100, 50));
+            Console.WriteLine(DataService.Subtraction(100, 50));
+            Console.WriteLine(DataService.Multiplication(100, 50));
+            Console.WriteLine(DataService.Division(100, 50));
+            Console.ReadKey();
+        }
+    }
+}
