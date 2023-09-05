3300727e0f0eacfe19949de16939b5a19f023764
iwsjlt <zed-n.v@mail.ru>
2023-09-05 21:01:02 +0500

    Task5

---
+++ Tyuiu.PomazDS.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PomazDS.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PomazDS.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ab0c2e0d-2d51-445f-a350-18f7c5e427fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PomazDS.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PomazDS.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.PomazDS.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(100, 50));
+            Console.WriteLine("A - B = " + DataService.Subtraction(100, 50));
+            Console.WriteLine("A * B = " + DataService.Multiplication(100, 50));
+            Console.WriteLine("A / B = " + DataService.Division(100, 0));
+            Console.ReadKey();
+        }
+    }
+}
